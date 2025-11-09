bbefa5d36bc0a9e0eb80dab0184541455d03d623
maxjb0 <150704498+maxjb0@users.noreply.github.com>
2025-11-09 02:40:46 +0000

    commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,7 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+  printf("Desafio criando cartas do jogos super trinfo");
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
